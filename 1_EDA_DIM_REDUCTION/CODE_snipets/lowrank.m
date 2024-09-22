fa83473f3ebc8d5f3787170fd6c85ddf3d182cd4
Joao Paulo Costeira <47594182+jpcosteira@users.noreply.github.com>
2024-09-22 21:58:53 +0100

    Update lowrank.m

--- 1_EDA_DIM_REDUCTION/CODE_snipets/lowrank.m
+++ 1_EDA_DIM_REDUCTION/CODE_snipets/lowrank.m
@@ -1,1 +1,21 @@
-
+%%
+% Linear Spaces SVD
+im=imread('lena.gif');
+imagesc(im);colormap(gray);
+im=double(im);
+[u,s,v]=svd(im,'econ');
+%% compare least squares with total least squares(SVD)
+r=randperm(256);
+for i=1:length(s)-1,
+    imr=zeros(size(im));
+    k=i;
+    sc=max(im(:));    
+    imr=u(:,1:k)*s(1:k,1:k)*v(:,1:k)';
+    b=im(:,r(1:i));
+    %projector
+    imr2=b*inv(b'*b)*b'*im;
+    imagesc([im, imr imr2]);
+    text(10,10,int2str(k));  
+    drawnow;
+    pause;
+end
